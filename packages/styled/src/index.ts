f05348df450d1ffad845d2dc898fa321ef2e6fe7
Christian Alfoni <christianalfoni@gmail.com>
2020-04-15 17:51:04 +0200

    add styled package

---
+++ packages/styled/src/index.ts
@@ -0,0 +1,76 @@
+import { IConfig, TCss } from "@stitches/css";
+import * as React from "react";
+import { Box, PolymorphicComponent } from "react-polymorphic-box";
+
+export type IStyled<
+  C extends IConfig,
+  E extends keyof JSX.IntrinsicElements
+> = (<P extends object = {}>(
+  defaults: P,
+  cb: (css: TCss<C>, props: P) => string
+) => PolymorphicComponent<P, E>) &
+  ((cb: (css: TCss<C>) => string) => PolymorphicComponent<{}, E>);
+
+export const createStyled = <C extends IConfig>() => {
+  let currentAs: string;
+
+  const context = React.createContext<TCss<C>>(null as any);
+  const useCssInstance = () => React.useContext(context);
+  const ProviderInstance: React.FC<{ css: TCss<C> }> = ({ css, children }) =>
+    React.createElement(
+      context.Provider,
+      {
+        value: css,
+      },
+      children
+    );
+  const styledInstance = (...args: any[]) => {
+    const as = currentAs;
+    return (props: any) => {
+      const css = useCssInstance();
+      if (!css) {
+        throw new Error(
+          "@stitches/styled - You do not seem to have added the Provider, please read the documentation for more help"
+        );
+      }
+      const cb = args.length === 1 ? args[0] : args[1];
+      const defaults = args.length === 1 ? {} : args[0];
+      const propsToPass = Object.keys(props).reduce<any>((aggr, key) => {
+        if (key in defaults) {
+          return aggr;
+        }
+
+        aggr[key] = props[key];
+
+        return aggr;
+      }, {});
+      return React.createElement(Box, {
+        as,
+        className: cb(css, { ...defaults, ...props }),
+        ...propsToPass,
+      });
+    };
+  };
+
+  const styledProxy = new Proxy(
+    {},
+    {
+      get(_, prop) {
+        currentAs = String(prop);
+        return styledInstance;
+      },
+    }
+  ) as {
+    [E in keyof JSX.IntrinsicElements]: IStyled<C, E>;
+  };
+
+  return {
+    Provider: ProviderInstance,
+    useCss: useCssInstance,
+    styled: styledProxy,
+  };
+};
+
+const { Provider, useCss, styled } = createStyled<{}>();
+
+export { Provider, useCss, styled };
