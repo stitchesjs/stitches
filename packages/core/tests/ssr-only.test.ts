34494b3da35e2b73c342023480a9872757a4e753
Hyeseong Kim <cometkim.kr@gmail.com>
2020-09-10 00:47:58 +0900

    remove a duplicated test case

--- packages/core/tests/ssr-only.test.ts
+++ packages/core/tests/ssr-only.test.ts
@@ -31,23 +31,3 @@ describe('createCss: SSR', () => {
     expect(styles).toEqual(secondStyles);
   });
 });
-
-// https://github.com/modulz/stitches/issues/200
-describe('#200 - getStyles() does not close keyframes block', () => {
-  test('should closes keyframe block on getStyles()', () => {
-    const css = createCss({}, null);
-    const atoms = css.keyframes({
-      '0%': { transform: 'scale(1)' },
-      '100%': { transform: 'scale(1.5)' },
-    }) as any;
-    // this acts like a request on the server
-    const { styles } = css.getStyles(() => {
-      atoms.toString();
-    });
-
-    expect(styles[1]).toMatchInlineSnapshot(`
-      "/* STITCHES */
-      @keyframes hIMhiY {0% {transform: scale(1);}100% {transform: scale(1.5);}}"
-    `);
-  });
-});
