051e70bc73c55e6b6e8afc8d67faef9eabbb1cea
Jonathan Neal <jonathantneal@hotmail.com>
2020-12-21 11:12:55 -0500

    Support underscore as a pseudo-colon

--- packages/core/src/lib/getSplitSelector.ts
+++ packages/core/src/lib/getSplitSelector.ts
@@ -1,4 +1,4 @@
 const getSplitSelector = (selector: string) =>
-	selector.split(/\s*,\s*/).map(part => part.replace(/^_+/, pseudo => ':'.repeat(pseudo.length)))
+	selector.split(/\s*,\s*/).map(part => part.replace(/^_+/, pseudo => '&' + ':'.repeat(pseudo.length)))
 
 export default getSplitSelector
