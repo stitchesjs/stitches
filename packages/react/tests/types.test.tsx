4e9d65e72139a355ff4dd1bc058dce7bf28e305a
Abdulhadi Hallak <hallak.aa@gmail.com>
2021-02-09 08:33:10 +0400

    Abdul.types.4 (#337)
    * update types
    
    * more cleanup
    
    * with selective as prop
    
    * :check
    
    * hmmm
    
    * another one
    
    * lkjlkk
    
    * cleanup
    
    * fix package.json

---
+++ packages/react/tests/types.test.tsx
@@ -0,0 +1,158 @@
+import * as React from 'react'
+import createStyled from '../types/index.d'
+
+const { styled } = createStyled({
+	conditions: {
+		large: '',
+	},
+})
+
+const Button = styled('button', {
+	variants: {
+		variant: {
+			red: {
+				backgroundOrigin: '',
+			},
+		},
+	},
+})
+
+/** TODO: Fix extending stitches components */
+const ExtendedButton = styled('button', {
+	variants: {
+		variant: {
+			red: {},
+		},
+		size: {
+			large: {},
+		},
+	},
+})
+
+/* -------------------------------------------------------------------------------------------------
+ * Extended Button using react utilities without polymorphism
+ * -----------------------------------------------------------------------------------------------*/
+
+const ExtendedButtonUsingReactUtils = React.forwardRef<React.ElementRef<typeof Button>, React.ComponentProps<typeof Button>>((props, forwardedRef) => {
+	return <Button />
+})
+
+/* -------------------------------------------------------------------------------------------------
+ * Extended Button using react utilities without polymorphism and inline `as`
+ * -----------------------------------------------------------------------------------------------*/
+export function ExtendedButtonUsingReactUtilsWithInternalInlineAs(props: React.ComponentProps<typeof Button>) {
+	/* Should not error with inline `as` component */
+	return <Button as={(_props) => <a {..._props} />} />
+}
+
+/* -------------------------------------------------------------------------------------------------
+ * Extended Polymorphic Button
+ * -----------------------------------------------------------------------------------------------*/
+
+/* -------------------------------------------------------------------------------------------------
+ * Normal Link
+ * -----------------------------------------------------------------------------------------------*/
+
+type LinkProps = React.ComponentProps<'a'> & {
+	isPrimary?: boolean
+	onToggle?(open: boolean): void
+	isDisabled?: boolean
+}
+
+const Link = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
+	const { children, isPrimary, ...linkProps } = props
+	return (
+		<a className={isPrimary ? 'primary' : undefined} ref={ref} {...linkProps}>
+			{children}
+		</a>
+	)
+})
+
+/* -----------------------------------------------------------------------------------------------*/
+export function Test() {
+	return (
+		<>
+			{/* Link accepts onToggle prop */}
+			<Link onToggle={(open) => console.log(open)} />
+
+			{/* Link accepts isPrimary prop */}
+			<Link isPrimary />
+
+			{/* Button does not accept href prop */}
+			{/* @ts-expect-error */}
+			<Button href="" />
+
+			{/* Button accepts form prop */}
+			<Button form="form" onClick={(e) => {}} />
+
+			{/* Button accepts css prop */}
+			<Button css={{ backgroundColor: 'ActiveCaption', padding: 'inherit' }} />
+
+			{/* Button accepts isDisabled prop */}
+			<Button variant="red" />
+
+			{/* Button accepts a responsive variant */}
+			<Button variant={{ large: 'red' }} />
+
+			{/* Button as "a" accepts href prop */}
+			<Button as="a" href="f" />
+
+			{/* Button as "a" does not accept form prop */}
+			{/* @ts-expect-error */}
+			<Button as="a" form="form" />
+
+			{/* Button as Link accepts href prop */}
+			<Button as={Link} href="#" />
+
+			{/* Button as Link accepts isPrimary prop */}
+			<Button as={Link} isPrimary />
+
+			{/* Button as Link accepts variant prop */}
+			<Button as={Link} isDisabled />
+
+			{/* Button as Link does not accept form prop */}
+			{/* @ts-expect-error */}
+			<Button as={Link} form="form" css={{ backgroundColor: 'ActiveCaption' }} />
+
+			{/* Button accepts onClick prop */}
+			<Button onClick={(event) => event.currentTarget.form} />
+
+			{/* Button as "a" accepts onClick prop */}
+			<Button as="a" onClick={(event) => event.currentTarget.href} />
+
+			{/* Button as Link accepts onClick prop, but it must be explicitly typed */}
+			<Button as={Link} onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => event.altKey} />
+
+			{/* ExtendedButton accepts variant prop */}
+			<ExtendedButton variant="red" />
+
+			{/* ExtendedButton accepts size prop */}
+			<ExtendedButton size="large" />
+
+			{/* ExtendedButton is typed as a button */}
+			<ExtendedButton
+				onClick={(e) => {
+					console.log(e.currentTarget.form)
+				}}
+			/>
+
+			{/* ExtendedButton as a has a href prop on the currentTraget */}
+			<ExtendedButton
+				as="a"
+				onClick={(e) => {
+					console.log(e.currentTarget.href)
+				}}
+			/>
+
+			{/* ExtendedButtonUsingReactUtils accepts isDisabled prop */}
+			<ExtendedButtonUsingReactUtils variant="red" />
+
+			{/* ExtendedButtonUsingReactUtils accepts onClick prop */}
+			<ExtendedButtonUsingReactUtils onClick={(event) => event.currentTarget.form} />
+
+			{/* ExtendedButtonUsingReactUtils does not accept as prop */}
+			{/* @ts-expect-error */}
+			<ExtendedButtonUsingReactUtils as="a" isDisabled />
+		</>
+	)
+}
---
+++ packages/react/types/index.d.ts
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import { StitchesCSS, TConditions, TTheme, TStyledSheet, VariantsCall, TStyledSheetFactory, IConfig } from '@stitches/core'
+
+/* Utils:
+ * -----------------------------------------------------------------------------------------------*/
+// abuse Pick to strip the call signature from ForwardRefExoticComponent
+type IntrinsicElementsKeys = keyof JSX.IntrinsicElements
+type ComponentInfer<T> = T extends IntrinsicElementsKeys | React.ComponentType<any> ? T : never
+export type ExtractVariantsFromComponent<T> = T extends StitchesComponent<any, infer V> ? V : {}
+
+/* StitchesComponent:
+ * -----------------------------------------------------------------------------------------------*/
+// abuse Pick to strip the call signature from ForwardRefExoticComponent
+type ForwardRefExoticBase<P> = Pick<React.ForwardRefExoticComponent<P>, keyof React.ForwardRefExoticComponent<any>>
+
+export interface StitchesComponent<DefaultElement, Variants = {}, Conditions = {}, Theme = {}, Utils = {}>
+	extends ForwardRefExoticBase<
+		Omit<React.ComponentPropsWithRef<DefaultElement>, keyof Variants | 'css' | 'as'> & {
+			css?: StitchesCSS<Conditions, Theme, Utils>
+		} & VariantsCall<Variants, Conditions>
+	> {
+	/** TODO: Decide which and how many elements we want to give their own overload */
+	(props: StitchesPropsWithAs<'div', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'code', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'pre', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'span', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'a', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'p', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'li', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'ul', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'image', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'table', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'td', Variants, Conditions, Theme, Utils>): JSX.Element
+	(props: StitchesPropsWithAs<'tr', Variants, Conditions, Theme, Utils>): JSX.Element
+	<As extends React.ComponentType>(props: StitchesPropsWithAs<As, Variants, Conditions, Theme, Utils>): JSX.Element
+	(
+		props: VariantsCall<Variants, Conditions> & { as?: DefaultElement } & React.ComponentPropsWithRef<DefaultElement> & {
+				css?: StitchesCSS<Conditions, Theme, Utils>
+			},
+	): JSX.Element
+}
+
+export type StitchesProps<Elm, Variants = {}, Conditions = {}, Theme = {}, Utils = {}> = VariantsCall<Variants, Conditions> &
+	Omit<React.ComponentPropsWithRef<Elm>, keyof Variants | 'css'> & {
+		css?: StitchesCSS<Conditions, Theme, Utils>
+	}
+
+type StitchesPropsWithAs<Elm, Variants = {}, Conditions = {}, Theme = {}, Utils = {}> = VariantsCall<Variants, Conditions> & { as: Elm } & Omit<React.ComponentPropsWithRef<Elm>, keyof Variants | 'css'> & {
+		css?: StitchesCSS<Conditions, Theme, Utils>
+	}
+
+export type IntrinsicElement<T, B = React.ElementRef<T>> = { [k in keyof HTMLElementTagNameMap]: HTMLElementTagNameMap[k] extends B ? k : never }[keyof HTMLElementTagNameMap]
+
+export type NonIntrinsicElementProps<T> = IntrinsicElement<T> extends never ? React.ComponentProps<T> : Omit<React.ComponentProps<T>, keyof React.ComponentProps<IntrinsicElement<T>>>
+
+/* Styled instance:
+ * -----------------------------------------------------------------------------------------------*/
+export type StyledInstance<Conditions = {}, Theme extends TTheme = {}, Utils = {}> = {
+	<E, Variants>(
+		elm: ComponentInfer<E>,
+		styles: StitchesCSS<Conditions, Theme, Utils> & {
+			variants?: { [k in keyof Variants]: { [b in keyof Variants[k]]: StitchesCSS<Conditions, Theme, Utils> } }
+		},
+	): StitchesComponent<E, Variants & ExtractVariantsFromComponent<E>, Conditions, Theme, Utils>
+}
+
+type ReactFactory = <Conditions extends TConditions = {}, Theme extends TTheme = {}, Utils = {}, Prefix = ''>(
+	_config?: IConfig<Conditions, Theme, Utils, Prefix>,
+) => TStyledSheet<Conditions, Theme, Utils> & { styled: StyledInstance<Conditions, Theme, Utils> }
+
+declare const styled: ReactFactory
+export default styled
