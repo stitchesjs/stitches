1076ac49d1a8fdb21e7ab7bdf7647a359ff900de
Jonathan Neal <jonathantneal@hotmail.com>
2021-02-08 20:29:54 -0500

    Remove underscore selectors

--- packages/core/src/getResolvedSelectors.js
+++ packages/core/src/getResolvedSelectors.js
@@ -1,19 +1,18 @@
 /** Returns selectors resolved from parent selectors and nested selectors. */
 const getResolvedSelectors = (
-	/** Parent selectors (e.g. '["a", "button"]`). */
+	/** Parent selectors (e.g. `["a", "button"]`). */
 	parentSelectors,
 	/** Nested selectors (e.g. `["&:hover", "&:focus"]`). */
 	nestedSelectors,
-) =>
-	parentSelectors.reduce((resolvedSelectors, parentSelector) => {
-		resolvedSelectors.push(
-			...nestedSelectors.map((selector) =>
-				selector.replace(/(^[^&]*)(&)([^]*$)|(^_+)([^]*$)|^[^]*$/, (plain, beforeNesting, duringNesting, afterNesting, duringPseudo, afterPseudo) =>
-					duringNesting ? beforeNesting + parentSelector + afterNesting : duringPseudo ? parentSelector + ':'.repeat(duringPseudo.length) + afterPseudo : parentSelector + ' ' + plain,
-				),
-			),
-		)
-		return resolvedSelectors
-	}, [])
+) => parentSelectors.reduce((resolvedSelectors, parentSelector) => {
+	resolvedSelectors.push(
+		...nestedSelectors.map((selector) =>
+			/&/.test(selector)
+				? selector.replace(/&/, parentSelector)
+				: selector.replace(/^/, parentSelector + ' '),
+		),
+	)
+	return resolvedSelectors
+}, [])
 
 export default getResolvedSelectors
