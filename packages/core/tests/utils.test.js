580293fc993ffd307adc72e7d869c4808867e2c3
Jonathan Neal <jonathantneal@hotmail.com>
2021-02-10 11:04:33 -0500

    test: separate conditions, prefix, utils tests

---
+++ packages/core/tests/utils.test.js
@@ -0,0 +1,23 @@
+import createCss from '../src/index.js'
+
+describe('Utils', () => {
+	let stitches, component
+
+	test('Authors can define utilties applied to components', () => {
+		stitches = createCss({
+			utils: {
+				bg: (value) => ({ backgroundColor: value }),
+			},
+		})
+
+		component = stitches.css({
+			bg: 'red',
+		})
+
+		expect(stitches.toString()).toBe('')
+
+		component.toString()
+
+		expect(stitches.toString()).toBe('.sxxl9ux{background-color:red;}')
+	})
+})
