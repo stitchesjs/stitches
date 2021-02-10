8a48c65471ef8bbdddc1c7a48d351147bd6cdc4d
Abdulhadi Hallak <hallak.aa@gmail.com>
2021-02-10 17:32:30 +0400

    migrate types (#340)

---
+++ packages/core/tests/types.test.ts
@@ -0,0 +1,86 @@
+//core types.tests.ts
+import createCss, { StitchesCss } from '../types/index.d'
+const css = createCss({
+	theme: {
+		colors: {
+			hiContrast: 'hsl(200, 12%, 5%)',
+			loContrast: 'white',
+			gray100: 'hsl(206, 20%, 98.8%)',
+			gray200: 'hsl(206, 14%, 96.0%)',
+			gray300: 'hsl(206, 13%, 93.7%)',
+			gray400: 'hsl(206, 12%, 92.0%)',
+			gray500: 'hsl(206, 12%, 89.5%)',
+			gray600: 'hsl(206, 11%, 85.2%)',
+			gray700: 'hsl(206, 10%, 80.0%)',
+			gray800: 'hsl(206, 6%, 56.1%)',
+			gray900: 'hsl(206, 6%, 43.9%)',
+			pedro: '$gray100',
+		},
+		space: {
+			1: '10px',
+			2: '20px',
+		},
+		fontSizes: {
+			'1': '11px',
+			'2': '13px',
+			'3': '15px',
+			'4': '17px',
+			'5': '19px',
+			'6': '21px',
+			'7': '27px',
+			'8': '35px',
+			'9': '59px',
+		},
+	},
+	conditions: {
+		bp1: '@media (min-width: 620px)',
+	},
+})
+
+css.keyframes({
+	from: {
+		color: '-moz-initial',
+	},
+	to: {
+		backgroundColor: '-moz-initial',
+	},
+})
+
+css.global({
+	hello: {
+		when: {
+			bp1: {
+				backgroundColor: '$1',
+			},
+		},
+		backgroundColor: '$font100',
+	},
+})
+
+type CSS = StitchesCss<typeof css>
+
+const ExternalStyles: CSS = {
+	when: {
+		bp1: {
+			backgroundColor: '$1',
+		},
+	},
+	backgroundColor: '$1',
+}
+
+const PotatoButton = css.css({})
+
+PotatoButton({
+	className: '',
+	css: {
+		backgroundColor: '$red100',
+		when: {
+			initial: {
+				backgroundColor: '-moz-initial',
+			},
+			bp1: {
+				backgroundColor: '-moz-initial',
+			},
+		},
+	},
+})
