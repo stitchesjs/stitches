a0ab09640eb7140c55dffaa15d3694ec51f25123
Jonathan Neal <jonathantneal@hotmail.com>
2021-02-11 12:00:43 -0500

    docs: share the wonderful story of getHashString

--- packages/core/src/getHashString.js
+++ packages/core/src/getHashString.js
@@ -5,6 +5,7 @@ export default (
 	/** Hashable object. */
 	object,
 ) => {
+	/** @see https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript/52171480?#comment115384813_52171480 */
 	for (var value = JSON.stringify(object), index = value.length, hash = 9; index; ) {
 		hash = Math.imul(hash ^ value.charCodeAt(--index), 9 ** 9)
 	}
