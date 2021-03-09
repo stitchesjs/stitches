11e91f9c1ef0cb7473b6440f1b51110bbfa3acfa
Jonathan Neal <jonathantneal@hotmail.com>
2021-03-08 20:28:58 -0500

    fix: keyframes expressions (#458)

---
+++ packages/core/tests/keyframes.js
@@ -0,0 +1,21 @@
+import createCss from '../src/index.js'
+
+describe('Keyframes', () => {
+	test('Expected behavior for the keyframes() method', () => {
+		const { keyframes, toString } = createCss()
+
+		const myKeyframes = keyframes({
+			'0%': {
+				opacity: '0',
+			},
+			'1%': {
+				opacity: '1',
+			},
+		})
+
+		expect(toString()).toBe('')
+		expect(`animation: 1s ${myKeyframes};`).toBe('animation: 1s sxk7pfs;')
+		expect(toString()).toBe('@keyframes sxk7pfs{0%{opacity:0;}1%{opacity:1;}}')
+		expect(myKeyframes.name).toBe('sxk7pfs')
+	})
+})
