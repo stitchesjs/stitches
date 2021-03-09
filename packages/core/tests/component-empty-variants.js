4ad2b076a51f57ad06f3698f728be5a2ab56c44b
Jonathan Neal <jonathantneal@hotmail.com>
2021-03-09 18:09:41 -0500

    fix: empty variant nesting (#468)

---
+++ packages/core/tests/component-empty-variants.js
@@ -0,0 +1,56 @@
+import createCss from '../src/index.js'
+
+describe('Empty Variants', () => {
+	test('Empty Variants', () => {
+		const { css, toString } = createCss()
+
+		css({
+			variants: {
+				size: {
+					xl: {},
+				},
+				tone: {
+					primary: {},
+				},
+			},
+			compoundVariants: [
+				{
+					tone: 'primary',
+					size: 'xl',
+				},
+			],
+		})({
+			tone: 'primary',
+			size: { initial: 'xl' },
+		})
+
+		expect(toString()).toBe('')
+	})
+
+	test('Empty Variants', () => {
+		const { css, toString } = createCss()
+
+		css({
+			variants: {
+				size: {
+					xl: {},
+				},
+				tone: {
+					primary: {},
+				},
+			},
+			compoundVariants: [
+				{
+					tone: 'primary',
+					size: 'xl',
+					css: { fontSize: '24px', color: 'black' },
+				},
+			],
+		})({
+			tone: 'primary',
+			size: { initial: 'xl' },
+		})
+
+		expect(toString()).toBe('@media all{.sxwklokr8fj7--c2{font-size:24px;color:black;}}')
+	})
+})
