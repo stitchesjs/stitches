337238e3b4a17f74acfbda244b0fdaf24b8a30fa
Jonathan Neal <jonathantneal@hotmail.com>
2021-03-23 21:51:00 -0400

    feat: add box-decoration-break polyfill

--- packages/core/src/createStringify.js
+++ packages/core/src/createStringify.js
@@ -15,6 +15,7 @@ const polys = {
 	appearance: (d) => ({ WebkitAppearance: d, appearance: d }),
 	backfaceVisibility: (d) => ({ WebkitBackfaceVisibility: d, backfaceVisibility: d }),
 	backgroundClip: (d) => ({ WebkitBackgroundClip: d, backgroundClip: d }),
+	boxDecorationBreak: (d) => ({ WebkitBoxDecorationBreak: d, boxDecorationBreak: d }),
 	clipPath: (d) => ({ WebkitClipPath: d, clipPath: d }),
 	content: (d) => ({ content: !/^([^]*["'][^]*|[A-Za-z]+\([^]*|[^]*-quote|inherit|initial|none|normal|revert|unset)$/.test(d) ? `"${d}"` : d }),
 	hyphens: (d) => ({ WebkitHyphens: d, hyphens: d }),
