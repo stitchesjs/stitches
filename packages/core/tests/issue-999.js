1b35f3c0a3927d49fd1066320b2127434726a092
Jonathan Neal <jonathantneal@hotmail.com>
2021-03-25 13:01:01 -0400

    wip: update test to include 3 examples

--- packages/core/tests/issue-999.js
+++ packages/core/tests/issue-999.js
@@ -1,7 +1,25 @@
 import { createCss } from '../src/index.js'
 
 describe('Issue #519', () => {
-	test('xyz', () => {
+	test('locally scoped token works 1 time', () => {
+		const { css, toString } = createCss({})
+
+		css({
+			$$syntax: 'red',
+
+			h1: {
+				color: '$$syntax',
+			},
+		})()
+
+		// prettier-ignore
+		expect(toString()).toBe(
+			'.sxuuu2e{---syntax:red;}' +
+			'.sxuuu2e h1{color:var(---syntax);}'
+		)
+	})
+
+	test('locally scoped token works 2 times', () => {
 		const { css, toString } = createCss({})
 
 		css({
@@ -21,6 +39,34 @@ describe('Issue #519', () => {
 			'.sxyrd68{---syntax:red;}' +
 			'.sxyrd68 h1{color:var(---syntax);}' +
 			'.sxyrd68 h2{color:$$syntax;}'
+		)
+	})
+
+	test('locally scoped token works 3 times', () => {
+		const { css, toString } = createCss({})
+
+		css({
+			$$syntax: 'red',
+
+			h1: {
+				color: '$$syntax',
+			},
+
+			h2: {
+				color: '$$syntax',
+			},
+
+			h3: {
+				color: '$$syntax',
+			},
+		})()
+
+		// prettier-ignore
+		expect(toString()).toBe(
+			'.sx4gdx9{---syntax:red;}' +
+			'.sx4gdx9 h1{color:var(---syntax);}' +
+			'.sx4gdx9 h2{color:$$syntax;}' +
+			'.sx4gdx9 h3{color:$$syntax;}'
 		)
 	})
 })
