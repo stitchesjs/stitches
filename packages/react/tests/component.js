7d90aa911adf2e9018710c4205d314f3329f50ca
Rafael Pedicini <rafael@rafgraph.dev>
2021-04-22 23:11:47 -0400

    Update tests

--- packages/react/tests/component.js
+++ packages/react/tests/component.js
@@ -2,35 +2,51 @@ import * as React from 'react'
 import createCss from '../src/index.js'
 
 describe('Components', () => {
-	test('The `styled` function returns an implicit span component', () => {
+	test('The `styled` function returns a React forwarded ref object', () => {
 		const { styled } = createCss()
 		const component = styled()
+		const expression = component.render()
 
 		expect(component.$$typeof).toBe(Symbol.for('react.forward_ref'))
-		expect(component.stitchesType).toBe('span')
+		expect(component.render).toBeInstanceOf(Function)
+		expect(React.isValidElement(expression)).toBe(true)
 	})
 
-	test('The `styled` function can return an explicit div component', () => {
+	test('The `styled` function creates an implicit span component', () => {
+		const { styled } = createCss()
+		const component = styled()
+		const expression = component.render()
+
+		expect(component.stitchesType).toBe('span')
+		expect(React.isValidElement(expression)).toBe(true)
+		expect(expression.type).toBe('span')
+	})
+
+	test('The `styled` function can create an explicit div component', () => {
 		const { styled } = createCss()
 		const component = styled('div')
+		const expression = component.render()
 
-		expect(component.$$typeof).toBe(Symbol.for('react.forward_ref'))
 		expect(component.stitchesType).toBe('div')
+		expect(React.isValidElement(expression)).toBe(true)
+		expect(expression.type).toBe('div')
 	})
 
-	test('The `styled` function can return an explicit React component', () => {
+	test('The `styled` function can create an explicit React component', () => {
 		function TextComponent() {
 			return 'text'
 		}
 
 		const { styled } = createCss()
 		const component = styled(TextComponent)
+		const expression = component.render()
 
-		expect(component.$$typeof).toBe(Symbol.for('react.forward_ref'))
 		expect(component.stitchesType).toBe(TextComponent)
+		expect(React.isValidElement(expression)).toBe(true)
+		expect(expression.type).toBe(TextComponent)
 	})
 
-	test('The `styled` function can return an explicit forwarded ref React component', () => {
+	test('The `styled` function can create an explicit forwarded ref React component', () => {
 		const ForwardedComponent = {
 			$$typeof: Symbol.for('react.forward_ref'),
 			render: () => 'text',
@@ -40,13 +56,12 @@ describe('Components', () => {
 		const component = styled(ForwardedComponent)
 		const expression = component.render()
 
-		expect(component.$$typeof).toBe(Symbol.for('react.forward_ref'))
 		expect(component.stitchesType).toBe(ForwardedComponent)
-		expect(expression.$$typeof).toBe(Symbol.for('react.element'))
+		expect(React.isValidElement(expression)).toBe(true)
 		expect(expression.type.$$typeof).toBe(Symbol.for('react.forward_ref'))
 	})
 
-	test('The `styled` function can return an explicit React memo component', () => {
+	test('The `styled` function can create an explicit React memo component', () => {
 		const MyComp = () => null
 		const MyCompMemo = React.memo(MyComp)
 
@@ -54,9 +69,17 @@ describe('Components', () => {
 		const component = styled(MyCompMemo)
 		const expression = component.render()
 
-		expect(component.$$typeof).toBe(Symbol.for('react.forward_ref'))
 		expect(component.stitchesType).toBe(MyCompMemo)
-		expect(expression.$$typeof).toBe(Symbol.for('react.element'))
+		expect(React.isValidElement(expression)).toBe(true)
 		expect(expression.type.$$typeof).toBe(Symbol.for('react.memo'))
+	})
+
+	test('The `styled` function creates a React element with key: null', () => {
+		const { styled } = createCss()
+		const component = styled()
+		const expression = component.render()
+
+		expect(React.isValidElement(expression)).toBe(true)
+		expect(expression.key).toBe(null)
 	})
 })
