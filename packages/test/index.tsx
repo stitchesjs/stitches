696ed2a5dbabbb2cde0404e63d501862963030bb
Pedro Duarte <contact@peduarte.com>
2021-08-12 20:19:11 +0200

    Add use case and instructions to test

--- packages/test/index.tsx
+++ packages/test/index.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 const { css, config, styled } = Stitches.createStitches({
 	utils: {
 		/** util to do stuff */
-		ms: (marginX: number) => ({ marginLeft: marginX }),
+		ml: (value: Stitches.PropertyValue<'margin'>) => ({ marginLeft: value }),
 	},
 	theme: {
 		colors: {
@@ -13,6 +13,20 @@ const { css, config, styled } = Stitches.createStitches({
 	},
 	themeMap: { ...Stitches.defaultThemeMap },
 })
+
+type CSS = Stitches.CSS<typeof config>
+
+// ---------------------------------------------------------------------------
+// INSTRUCTIONS:
+// Test cases are below.
+// Make sure to only have one uncommented use case at once.
+// Make sure to "Restart TS Server".
+// Be patient, some errors only show up after 30+ seconds.
+// ---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
+// USE CASE 1
+// ---------------------------------------------------------------------------
 
 const StyledStatus = Stitches.styled('span', {
 	color: 'red',
@@ -32,3 +46,14 @@ export const Status: React.ForwardRefExoticComponent<React.ComponentProps<typeof
 export default function Home() {
 	return <Status onClick={event => { }} />
 }
+
+// ---------------------------------------------------------------------------
+// USE CASE 2
+// Uncomment to test
+// ---------------------------------------------------------------------------
+
+// const shared: CSS = {
+// 	color: '$red100'
+// }
+
+// const Text = styled('span', shared)
