9596d557cbc1b2feeb11148449423bee95c4a687
Lucas Castro <lucas.castro@gmail.com>
2021-09-06 20:32:30 -0400

    fix(754): Resolved TS issue on VariantProps

--- packages/react/types/index.d.ts
+++ packages/react/types/index.d.ts
@@ -2,7 +2,7 @@ import type Stitches from './stitches'
 
 import type * as Config from './config'
 import type * as CSSUtil from './css-util'
-import type * as StyledComponent from './styled-component'
+import type * as S from './styled-component'
 
 export type CreateStitches = Config.CreateStitches
 export type CSSProperties = CSSUtil.CSSProperties
@@ -39,7 +39,12 @@ export type PropertyValue<K extends keyof CSSUtil.CSSProperties> = { readonly [C
 export type ScaleValue<K> = { readonly [CSSUtil.$$ScaleValue]: K }
 
 /** Returns a type that suggests variants from a component as possible prop values. */
-export type VariantProps<Component> = StyledComponent.TransformProps<Component[StyledComponent.$$StyledComponentProps], Component[StyledComponent.$$StyledComponentMedia]>
+export type VariantProps<
+  C extends S.StyledComponent<
+    C[S.$$StyledComponentType], C[S.$$StyledComponentProps], C[S.$$StyledComponentMedia]
+  >
+> = S.TransformProps<C[S.$$StyledComponentProps], C[S.$$StyledComponentMedia]>
+
 
 /** Map of CSS properties to token scales. */
 export declare const defaultThemeMap: DefaultThemeMap
