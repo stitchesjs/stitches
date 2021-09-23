c8615f77cbc886cbfac072ddc4d3fbdb40b6918b
Jonathan Neal <jonathantneal@hotmail.com>
2021-09-23 08:20:01 -0400

    Fix issue typing import and font-face rules (#809)

---
+++ packages/test/Issue-803-react.ts
@@ -0,0 +1,79 @@
+import { globalCss } from '@stitches/react'
+
+void globalCss({
+	"@font-face": {
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "100",
+		src: "url('/Roboto-Thin.ttf') format('truetype')"
+	},
+	body: {
+		color: 'ActiveText',
+	},
+})
+
+void globalCss({
+	"@font-face": [
+		{
+			fontFamily: "Roboto",
+			fontStyle: "normal",
+			fontWeight: "100",
+			src: "url('/Roboto-Thin.ttf') format('truetype')",
+		},
+	],
+	body: {
+		color: 'ActiveText',
+	},
+})
+
+void globalCss({
+	"@font-face": {
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "100",
+		src: "url('/Roboto-Thin.ttf') format('truetype')",
+	},
+	body: {
+		color: 'ActiveText',
+	},
+}, {
+	"@font-face": [
+		{
+			fontFamily: "Roboto",
+			fontStyle: "normal",
+			fontWeight: "100",
+			src: "url('/Roboto-Thin.ttf') format('truetype')",
+		},
+		{
+			fontFamily: "Roboto",
+			fontStyle: "normal",
+			fontWeight: "400",
+			src: "url('/Roboto-Regular.ttf') format('truetype')",
+		},
+	],
+	body: {
+		color: 'ActiveText',
+	},
+})
+
+void globalCss({
+	"@import": "some.css",
+	body: {
+		color: 'ActiveText',
+	},
+})
+
+void globalCss({
+	"@import": "some.css",
+	body: {
+		color: 'ActiveText',
+	},
+}, {
+	"@import": [
+		"some.css",
+		"someother.css",
+	],
+	body: {
+		color: 'ActiveText',
+	},
+})
