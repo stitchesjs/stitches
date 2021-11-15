2b173b01b6bb0bc722567bd4f2e9b1b6f7ed9564
Abdul <hallak.aa@gmail.com>
2021-11-15 21:14:47 +0400

    Add test for compound variants

--- packages/core/tests/issue-725.js
+++ packages/core/tests/issue-725.js
@@ -16,7 +16,7 @@ describe('Issue #725', () => {
 				},
 			},
 		})
-		const render = Flex({
+		Flex({
 			justify: {
 				'@tablet': 'end',
 				'@wide': 'end',
@@ -48,13 +48,50 @@ describe('Issue #725', () => {
 				color: 'red',
 			},
 		})
-		const render = Flex({
+		Flex({
 			justify: {
 				'@tablet': 'end',
 				'@wide': 'end',
-				'@mobile': 'start'
+				'@mobile': 'start',
 			},
 		})
-		expect(getCssText()).toBe(`--sxs{--sxs:3 c-PJLV-gmqXFB-color-red c-PJLV-jobbEJ-justify-end c-PJLV-kxjDkG-justify-start}@media{.c-PJLV-gmqXFB-color-red{color:red}@media (min-width: 720px), (min-width: 1536px){.c-PJLV-jobbEJ-justify-end{justify-content:flex-end}}@media (min-width: 420px){.c-PJLV-kxjDkG-justify-start{justify-content:flex-start}}}`)
+		expect(getCssText()).toBe(
+			`--sxs{--sxs:3 c-PJLV-gmqXFB-color-red c-PJLV-jobbEJ-justify-end c-PJLV-kxjDkG-justify-start}@media{.c-PJLV-gmqXFB-color-red{color:red}@media (min-width: 720px), (min-width: 1536px){.c-PJLV-jobbEJ-justify-end{justify-content:flex-end}}@media (min-width: 420px){.c-PJLV-kxjDkG-justify-start{justify-content:flex-start}}}`,
+		)
+	})
+	test('Test compound variants', () => {
+		const { css, getCssText } = createStitches({
+			media: {
+				bp1: `(min-width: 720px)`,
+				bp2: `(min-width: 1536px)`,
+			},
+		})
+
+		const Button = css({
+			variants: {
+				variant: {
+					red: { backgroundColor: 'tomato' },
+					blue: { backgroundColor: 'SkyBlue' },
+				},
+				disabled: {
+					true: { backgroundColor: 'gray' },
+				},
+			},
+			compoundVariants: [
+				{
+					variant: 'red',
+					disabled: true,
+					css: {
+						padding: 50,
+					},
+				},
+			],
+		})
+
+		Button({
+			variant: { '@bp1': 'red', '@bp2': 'red' },
+			disabled: { '@bp2': true, '@bp1': false },
+		})
+		expect(getCssText()).toBe(`--sxs{--sxs:3 c-PJLV-dJnmKC-disabled-true c-PJLV-gSmlSg-variant-red}@media{@media (min-width: 1536px){.c-PJLV-dJnmKC-disabled-true{background-color:gray}}@media (min-width: 720px), (min-width: 1536px){.c-PJLV-gSmlSg-variant-red{background-color:tomato}}}--sxs{--sxs:4 c-PJLV-elpmbs-cv}@media{@media (min-width: 1536px){@media (min-width: 720px), (min-width: 1536px){.c-PJLV-elpmbs-cv{padding:50px}}}}`)
 	})
 })
