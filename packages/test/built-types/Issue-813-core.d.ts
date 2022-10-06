6df414e36da70bad9e7c332a96fcb3776568ffff
Abdul <hallak.aa@gmail.com>
2022-10-06 17:33:53 +0400

    Update types

--- packages/test/built-types/Issue-813-core.d.ts
+++ packages/test/built-types/Issue-813-core.d.ts
@@ -16,9 +16,24 @@ export declare const config: {
             color: import("@stitches/core/types/css-util").WithScaleValue<"colors">;
         };
     };
-}, css: <Composers extends (string | import("@stitches/core/types/util").Function | {
-    [name: string]: unknown;
-})[], CSS = import("@stitches/core/types/css-util").CSS<{}, {
+}, css: {
+    withConfig: (config: {
+        componentId?: string | undefined;
+        displayName?: string | undefined;
+        shouldForwardStitchesProp?: ((prop: "css" | (string & {})) => boolean | void) | undefined;
+    }) => import("@stitches/core/types/stitches").CssFunctionType<{}, {
+        colors: {
+            primary: string;
+        };
+    }, import("@stitches/core/types/config").DefaultThemeMap, {
+        bg: (value: Stitches.PropertyValue<'backgroundColor'>) => {
+            color: import("@stitches/core/types/css-util").WithPropertyValue<"backgroundColor">;
+        };
+        c: (value: Stitches.ScaleValue<'colors'>) => {
+            color: import("@stitches/core/types/css-util").WithScaleValue<"colors">;
+        };
+    }>;
+} & import("@stitches/core/types/stitches").CssFunctionType<{}, {
     colors: {
         primary: string;
     };
@@ -29,18 +44,7 @@ export declare const config: {
     c: (value: Stitches.ScaleValue<'colors'>) => {
         color: import("@stitches/core/types/css-util").WithScaleValue<"colors">;
     };
-}>>(...composers: { [K in keyof Composers]: string extends Composers[K] ? Composers[K] : Composers[K] extends string | import("@stitches/core/types/util").Function ? Composers[K] : import("@stitches/core/types/stitches").RemoveIndex<CSS> & {
-    variants?: {
-        [x: string]: {
-            [x: string]: CSS;
-            [x: number]: CSS;
-        };
-    } | undefined;
-    compoundVariants?: (("variants" extends keyof Composers[K] ? Composers[K][keyof Composers[K] & "variants"] extends infer T ? { [Name in keyof T]?: import("@stitches/core/types/util").String | import("@stitches/core/types/util").Widen<keyof Composers[K][keyof Composers[K] & "variants"][Name]> | undefined; } : never : import("@stitches/core/types/util").WideObject) & {
-        css: CSS;
-    })[] | undefined;
-    defaultVariants?: ("variants" extends keyof Composers[K] ? Composers[K][keyof Composers[K] & "variants"] extends infer T_1 ? { [Name_1 in keyof T_1]?: import("@stitches/core/types/util").String | import("@stitches/core/types/util").Widen<keyof Composers[K][keyof Composers[K] & "variants"][Name_1]> | undefined; } : never : import("@stitches/core/types/util").WideObject) | undefined;
-} & CSS & (Composers[K] extends infer T_2 ? { [K2 in keyof T_2]: K2 extends "compoundVariants" | "defaultVariants" | "variants" ? unknown : K2 extends keyof CSS ? CSS[K2] : unknown; } : never); }) => import("@stitches/core/types/styled-component").CssComponent<import("@stitches/core/types/styled-component").StyledComponentType<Composers>, import("@stitches/core/types/styled-component").StyledComponentProps<Composers>, {}, CSS>;
+}>;
 export declare const colorValue1: Stitches.PropertyValue<'backgroundColor', typeof config>;
 export declare const colorValue2: Stitches.PropertyValue<'backgroundColor', typeof config>;
 export declare const colorToken: Stitches.ScaleValue<'colors', typeof config>;
