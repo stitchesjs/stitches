0382e2f40ed82f32b9a16db818701eaf18aea1b5
Nina Bondar <nina.bondar@voiceflow.com>
2024-01-04 15:54:15 -0500

    feat: optional theme :host attachment

--- packages/core/src/features/globalCss.js
+++ packages/core/src/features/globalCss.js
@@ -7,47 +7,41 @@ import { toHash } from '../convert/toHash.js'
 const createGlobalCssFunctionMap = createMemo()
 
 /** Returns a function that applies global styles. */
-export const createGlobalCssFunction = (
-	 config,
-	 sheet
-) => createGlobalCssFunctionMap(config, () => (
-	 ...styles
-) => {
-	const render = () => {
-		for (let style of styles) {
-			style = typeof style === 'object' && style || {}
+export const createGlobalCssFunction = (config, sheet) =>
+	createGlobalCssFunctionMap(config, () => (...styles) => {
+		const render = () => {
+			for (let style of styles) {
+				style = (typeof style === 'object' && style) || {}
 
-			let uuid = toHash(style)
+				let uuid = toHash(style)
 
-			if (!sheet.rules.global.cache.has(uuid)) {
-				sheet.rules.global.cache.add(uuid)
+				if (!sheet.rules.global.cache.has(uuid)) {
+					sheet.rules.global.cache.add(uuid)
 
-				// support @import rules
-				if ('@import' in style) {
-					let importIndex = [].indexOf.call(sheet.sheet.cssRules, sheet.rules.themed.group) - 1
+					// support @import rules
+					if ('@import' in style) {
+						let importIndex = [].indexOf.call(sheet.sheet.cssRules, sheet.rules.themed.group) - 1
 
-					// wrap import in quotes as a convenience
-					for (
-						let importValue of /** @type {string[]} */ ([].concat(style['@import']))
-					) {
-						importValue = importValue.includes('"') || importValue.includes("'") ? importValue : `"${importValue}"`
+						// wrap import in quotes as a convenience
+						for (let importValue of /** @type {string[]} */ ([].concat(style['@import']))) {
+							importValue = importValue.includes('"') || importValue.includes("'") ? importValue : `"${importValue}"`
 
-						sheet.sheet.insertRule(`@import ${importValue};`, importIndex++)
+							sheet.sheet.insertRule(`@import ${importValue};`, importIndex++)
+						}
+
+						delete style['@import']
 					}
 
-					delete style['@import']
+					toCssRules(style, [], [], config, (cssText) => {
+						sheet.rules.global.apply(cssText)
+					})
 				}
-
-				toCssRules(style, [], [], config, (cssText) => {
-					sheet.rules.global.apply(cssText)
-				})
 			}
+
+			return ''
 		}
 
-		return ''
-	}
-
-	return define(render, {
-		toString: render,
+		return define(render, {
+			toString: render,
+		})
 	})
-})
